00bc44ea62045484470de0cb96d566176fa09e5f
Davi Gabriel Marques de Lima - SENAI-Cuiba/MT <00618558@alunosenai.mt>
2026-06-19 10:03:29 -0400

    adição opção 1

--- contas.py
+++ contas.py
@@ -3,4 +3,5 @@ num2 = float(input("Digite o segundo numero: "))
 
 opcao = int(input("Escolha uma operação: \n 1 soma \n 2 subtração \n 3 divisão \n 4 multiplicação "))
 
-print("A soma é:", num + num2)
+if opcao == "1":
+    (print("A soma é: ", num + num2))
