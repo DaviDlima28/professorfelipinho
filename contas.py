d51c44c53184240c6263d36faa556d871167aa3f
Davi Gabriel Marques de Lima - SENAI-Cuiba/MT <00618558@alunosenai.mt>
2026-06-19 09:44:51 -0400

    NOVAS CONTAS

--- contas.py
+++ contas.py
@@ -1,4 +1,6 @@
 num = float(input("Digite um numero: "))
 num2 = float(input("Digite o segundo numero: "))
 
+opcao = int(input("Escolha uma operação: \n 1 soma \n 2 subtração \n 3 divisão \n 4 multiplicação "))
+
 print("A soma é:", num + num2)
