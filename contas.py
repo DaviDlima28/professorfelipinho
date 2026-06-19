ba47ff370187b52f5e321e641c019a9af2249799
Davi Gabriel Marques de Lima - SENAI-Cuiba/MT <00618558@alunosenai.mt>
2026-06-19 10:17:30 -0400

    repetição caso der errado

--- contas.py
+++ contas.py
@@ -1,13 +1,25 @@
 num = float(input("Digite um numero: "))
 num2 = float(input("Digite o segundo numero: "))
 
-opcao = int(input("Escolha uma operação: \n 1 soma \n 2 subtração \n 3 divisão \n 4 multiplicação "))
+while True:
+    print("Escolha uma operação: \n 1 soma \n 2 subtração \n 3 divisão \n 4 multiplicação ")
 
-if opcao == "1":
-    (print("A soma é: ", num + num2))
-elif opcao == "2":
-    (print("A subtração é: ", num - num2))
-elif opcao == "3":
-    (print("A divisão é: ", num / num2 ))
-elif opcao == "4":
-    (print("A multiplicação é ", num * num2))
+    opcao = input("Sua opção: ") 
+    
+    if opcao == "1":
+        (print("A soma é: ", num + num2))
+        break
+    elif opcao == "2":
+        (print("A subtração é: ", num - num2))
+        break
+    elif opcao == "3":
+        if num2 == 0:
+            print("Erro: Não é possível dividir por zero!")
+        else:
+            (print("A divisão é: ", num / num2 ))
+        break
+    elif opcao == "4":
+        (print("A multiplicação é ", num * num2))
+        break
+    else: 
+        print("opção invalida, escolha uma das 4 operações disponiveis")
